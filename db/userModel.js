72e8c8520726569760dab31436cd98bf76ffb3cf
Emanuel T <emanuel@tise.ro>
2022-08-21 18:28:20 +0300

    Boiler plate

---
+++ db/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Please provide an Email!"],
+        unique: [true, "Email Exist"],
+    },
+    phone: {
+        type: String,
+        required: false,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password!"],
+        unique: false,
+    },
+    username: {
+        type: String,
+        required: false,
+        unique: [true, "Username exists"],
+    },
+    headline: {
+        type: String,
+        required: false,
+        unique: false,
+    },
+    avatar: {
+        type: String,
+        required: false,
+        unique: false
+    }
+})
+module.exports = mongoose.model.Users || mongoose.model("Users", UserSchema);
